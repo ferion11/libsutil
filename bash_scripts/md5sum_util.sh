002b09b84de752386b3b56997886b53b397f8b6a
DanielDevBR <dados.linux@gmail.com>
2020-01-26 14:49:01 -0300

    added md5sum_util

---
+++ bash_scripts/md5sum_util.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+die() { echo >&2 "$*"; exit 1; };
+
+if [ -d "$1" ]; then
+	echo "* Making MD5SUMS file..."
+	cd "$1"
+	md5sum * > MD5SUMS || die "* ERROR: Can't generate MD5SUMS file"
+	echo "* MD5SUMS file done"
+else
+	die "* ERROR: Directory don't exist: $1"
+fi
