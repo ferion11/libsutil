05770d975e91952a62421ef20478b1ec352bab2b
DanielDevBR <dados.linux@gmail.com>
2020-01-28 15:56:20 -0300

    scripts to change Logos.sh

---
+++ bash_scripts/change_Logos_to_Appimage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+APPDIR="$HOME/AppImage"
+APPDIR_BIN="$APPDIR/bin"
+
+#------- making the start script -------
+IFS_TMP=$IFS
+IFS=$'\n'
+LOGOS_EXE=$(find $HOME/.wine32 -name Logos.exe |  grep "Logos\/Logos.exe")
+rm -rf $HOME/Desktop/Logos.sh
+
+cat > $HOME/Desktop/Logos.sh << EOF
+#!/bin/bash
+export PATH=$APPDIR_BIN:\$PATH
+# Save IFS
+IFS_TMP=\$IFS
+IFS=$'\n'
+
+LC_ALL=C wine "$LOGOS_EXE"
+
+# restore IFS
+IFS=\$IFS_TMP
+EOF
+
+chmod +x $HOME/Desktop/Logos.sh
+IFS=$IFS_TMP
+#------------------------------
