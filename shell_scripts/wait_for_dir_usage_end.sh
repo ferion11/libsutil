255040082e4181f228e0d0a8b903d18b596a3673
DanielDevBR <dados.linux@gmail.com>
2020-09-05 13:24:04 -0300

    sh POSIX improved wait_for_dir_usage_end.sh

--- shell_scripts/wait_for_dir_usage_end.sh
+++ shell_scripts/wait_for_dir_usage_end.sh
@@ -1,30 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
+# wait to all process that is using the ${1} directory to finish
 wait_process_using_dir() {
 	VERIFICATION_DIR="${1}"
 	VERIFICATION_TIME=7
-	VERIFICATION_TRIES=3
+	VERIFICATION_NUM=3
 
-	echo "* Starting wait_process_using_dir..."
-	for (( c=1; c<=${VERIFICATION_TRIES}; c++ ))
-	do
+	echo "* Starting winetricks-moded wait_process_using_dir..."
+	i=0 ; while true; do
+		i=$((i+1))
+		echo "-------"
+		echo "winetricks-moded: loop with i=${i}"
+
+		echo "winetricks-moded: sleep ${VERIFICATION_TIME}"
+		sleep "${VERIFICATION_TIME}"
+
 		PID_LIST="$(fuser "${VERIFICATION_DIR}")"
-		echo "PID_LIST: ${PID_LIST}"
+		echo "winetricks-moded PID_LIST: ${PID_LIST}"
+
 		# double quote make one bug!
 		# shellcheck disable=SC2086
 		FIST_PID="$(echo ${PID_LIST} | cut -d' ' -f1)"
-		echo "FIST_PID: ${FIST_PID}"
-		if [ -z "${FIST_PID}" ]; then
-			echo "sleep ${VERIFICATION_TIME}"
-			sleep "${VERIFICATION_TIME}"
-		else
-			c=1
-			echo "tail --pid=${FIST_PID} -f /dev/null"
+		echo "winetricks-moded FIST_PID: ${FIST_PID}"
+		if [ -n "${FIST_PID}" ]; then
+			i=0
+			echo "winetricks-moded: tail --pid=${FIST_PID} -f /dev/null"
 			tail --pid="${FIST_PID}" -f /dev/null
+			continue
 		fi
-		echo "end loop with c=${c}"
+
+		echo "-------"
+		[ "${i}" -lt "${VERIFICATION_NUM}" ] || break
 	done
-	echo "* End of wait_process_using_dir."
+	echo "* End of winetricks-moded wait_process_using_dir."
 }
 
-wait_process_using_dir "/home/admlocal/.wine32"
+wait_process_using_dir "${1}"
